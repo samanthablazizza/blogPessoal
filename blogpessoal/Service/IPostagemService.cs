6ec18dd8b463aaeaa6419fb202cb245f7a9cddc8
samanthablazizza <samantha.blazizza@gmail.com>
2023-09-22 17:04:02 -0300

    Blog Pessoal

---
+++ blogpessoal/Service/IPostagemService.cs
@@ -0,0 +1,17 @@
+﻿using blogpessoal.Model;
+
+namespace blogpessoal.Service
+{
+    public interface IPostagemService
+    {
+        Task<IEnumerable<Postagem>> GetAll();
+
+        Task<Postagem?> GetById(long id);
+
+        Task<IEnumerable<Postagem>> GetByTitulo(string titulo);
+
+        Task<Postagem?>Create(Postagem postagem);
+
+        Task Delete(Postagem postagem);
+    }
+}
